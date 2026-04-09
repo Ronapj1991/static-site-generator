3d78a0311c6641a8c763979ba142b318112837d3
Ronapj1991 <Ronapj1991@gmail.com>
2026-04-09 03:14:28 +0000

    add test for leafnode

---
+++ src/test_leafnode.py
@@ -0,0 +1,25 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+    
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_repr(self):
+        node = LeafNode("p", "Hello")
+        result = repr(node)
+        self.assertEqual(result, f"LeafNode(p, Hello, None)")
+
+if __name__ == "__main__":
+    unittest.main()
