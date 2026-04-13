10eb1e82f3f71c9cb647d47a92c5a9f23c87f50a
Ronapj1991 <Ronapj1991@gmail.com>
2026-04-13 16:52:26 +0000

    add text to html converter

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
-    PLAIN = "text"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -22,3 +23,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    t = text_node.text_type
+
+    if t == TextType.PLAIN:
+        return  LeafNode(None, text_node.text)
+    elif t == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif t == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif t == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif t == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif t == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError("Unsupported Type")
